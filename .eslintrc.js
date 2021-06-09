047aa7ddb813a811e15afa40f264bd68a2da9593
mohammadizanloo55 <mohammadizanloo55@gmail.com>
2021-06-09 15:20:54 +0430

    Added @fullstacksjs to eslint extends

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
 module.exports = {
-  extends: ["react-app", "react-app/jest"],
+  extends: ["react-app", "react-app/jest", "@fullstacksjs"],
+  rules: {
+    "react/no-unstable-nested-components": "off",
+    "import/no-relative-packages": "off",
+  },
 };
