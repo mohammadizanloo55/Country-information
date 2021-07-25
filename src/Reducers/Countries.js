3682b38d87c682be55553bf1a28aa491cc00f562
mohammadizanloo55 <mohammadizanloo55@gmail.com>
2021-07-25 10:51:45 +0430

    Added MergeNewData function to Countries Reducer

--- src/Reducers/Countries.js
+++ src/Reducers/Countries.js
@@ -1,12 +1,22 @@
-const SetNewData = ({ payload }) => {
+const SetNewData = (State, { payload }) => {
   return {
+    ...State,
     ...payload.Data,
+  };
+};
+const MergeNewData = (State, Action) => {
+  return {
+    ...State,
+    data: [...State.data, ...Action.payload.Data],
   };
 };
 const CountriesReducer = (State, Action) => {
   switch (Action.type) {
     case "SetNewData": {
-      return SetNewData(Action);
+      return SetNewData(State, Action);
+    }
+    case "MergeNewData": {
+      return MergeNewData(State, Action);
     }
     default: {
       console.error("You are not set the type for Action");
