c4b21796d86563a38ed6fc6503abcdc74a7fd3e6
mohammadizanloo55 <mohammadizanloo55@gmail.com>
2021-08-06 16:02:51 +0430

    Changed CountriesReducer for new State Model

--- src/Reducers/Countries.js
+++ src/Reducers/Countries.js
@@ -1,15 +1,52 @@
-const SetNewData = (State, { payload }) => {
-  return {
-    ...State,
-    ...payload.Data,
+const SetNewData = (State, Action) => {
+  const PrevState = { ...State };
+
+  const { IsDefault, NewData, ModeKey, QueryKey } = Action.payload;
+
+  PrevState.Mode = ModeKey;
+
+  if (IsDefault) {
+    PrevState[ModeKey] = {
+      ...PrevState[ModeKey],
+      Data: NewData.data,
+      PageLength: NewData.PageLength,
+    };
+
+    return PrevState;
+  }
+
+  PrevState.QueryKey = QueryKey;
+
+  PrevState[ModeKey][QueryKey] = {
+    Data: NewData.data,
+    PageLength: NewData.PageLength,
+    Page: 2,
   };
+
+  return PrevState;
 };
 const MergeNewData = (State, Action) => {
-  return {
-    ...State,
-    data: [...State.data, ...Action.payload.Data],
-  };
+  const PrevState = { ...State };
+
+  const { IsDefault, NewData, ModeKey, QueryKey } = Action.payload;
+  if (IsDefault) {
+    PrevState[ModeKey].Data = [...PrevState[ModeKey].Data, ...NewData.data];
+
+    ++PrevState[ModeKey].Page;
+
+    return PrevState;
+  }
+
+  PrevState[ModeKey][QueryKey].Data = [
+    ...PrevState[ModeKey][QueryKey].Data,
+    ...NewData.data,
+  ];
+
+  ++PrevState[ModeKey][QueryKey].Page;
+
+  return PrevState;
 };
+
 const CountriesReducer = (State, Action) => {
   switch (Action.type) {
     case "SetNewData": {
