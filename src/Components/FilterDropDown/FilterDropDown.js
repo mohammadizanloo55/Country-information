f40825ba771aae07fc86868a14befad5f7bb6e1c
mohammadizanloo55 <mohammadizanloo55@gmail.com>
2021-08-06 16:06:13 +0430

    Added FilterDropDown Component

---
+++ src/Components/FilterDropDown/FilterDropDown.js
@@ -0,0 +1,27 @@
+import {
+  Button,
+  Menu,
+  MenuButton,
+  MenuItemOption,
+  MenuList,
+  MenuOptionGroup,
+} from "@chakra-ui/react";
+import { memo } from "react";
+
+const FilterDropDown = ({ Options, Title, OnChange }) => (
+  <Menu closeOnSelect={true}>
+    <MenuButton as={Button} colorScheme="blue">
+      {Title}
+    </MenuButton>
+    <MenuList minWidth="240px">
+      <MenuOptionGroup onChange={(e) => OnChange(e)} type="radio">
+        {Options.map((Value) => (
+          <MenuItemOption key={Value} value={Value}>
+            {Value}
+          </MenuItemOption>
+        ))}
+      </MenuOptionGroup>
+    </MenuList>
+  </Menu>
+);
+export default memo(FilterDropDown);
