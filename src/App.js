0600d0107c6ed9684c9dec1f422dde599f6c933f
mohammadizanloo55 <mohammadizanloo55@gmail.com>
2021-08-06 16:11:05 +0430

    Added some custom global style and MainPage Route

--- src/App.js
+++ src/App.js
@@ -3,21 +3,32 @@ import loadable from "@loadable/component";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const Header = loadable(() => import("./Components/Header/Header"));
+const MainPage = loadable(() => import("./Router/MainPage/MainPage"));
 
 const ThemeConfig = {
   initialColorMode: "system",
   useSystemColorMode: false,
+  styles: {
+    global: {
+      "html, body, #root": {
+        height: "100% !important",
+      },
+      "input,textarea,button,select,a": {
+        WebkitTapHighlightColor: "transparent",
+      },
+    },
+  },
 };
 
 const App = () => {
-  const theme = extendTheme({ ThemeConfig });
+  const theme = extendTheme(ThemeConfig);
   return (
     <ChakraProvider theme={theme}>
       <Router>
         <Header />
         <Switch>
           <Route path="/" exact>
-            main page
+            <MainPage />
           </Route>
         </Switch>
       </Router>
