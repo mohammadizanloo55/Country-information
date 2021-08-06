cf9ebed099d871bd38e134fa88a662c07a789d0c
mohammadizanloo55 <mohammadizanloo55@gmail.com>
2021-08-06 15:58:30 +0430

    Added SearchInput Component

---
+++ src/Components/SearchInput/SearchInput.js
@@ -0,0 +1,30 @@
+import { Input, InputGroup, InputLeftElement, Text } from "@chakra-ui/react";
+import { memo } from "react";
+import { HiSearch } from "react-icons/hi";
+
+const SearchInput = ({ InputType, Placeholder, onChange }) => (
+  <InputGroup>
+    <InputLeftElement
+      pointerEvents="none"
+      children={
+        <Text color="gray.500" fontSize="2xl">
+          <HiSearch />
+        </Text>
+      }
+    />
+    <Input
+      onChange={onChange}
+      border="none"
+      boxShadow="md"
+      type={InputType}
+      placeholder={Placeholder}
+      size="lg"
+      _focus={{
+        border: "none",
+        boxShadow:
+          "0 10px 15px -3px rgba(0, 0, 0, 0.1),0 4px 6px -2px rgba(0, 0, 0, 0.05);",
+      }}
+    />
+  </InputGroup>
+);
+export default memo(SearchInput);
