adec1cdbfe950db7e16cc5225e26c867fb6fa6e6
mohammadizanloo55 <mohammadizanloo55@gmail.com>
2021-08-07 08:39:09 +0430

    Added useMemo for ThemeConfig

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import loadable from "@loadable/component";
+import { useMemo } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const Header = loadable(() => import("./Components/Header/Header"));
 const MainPage = loadable(() => import("./Router/MainPage/MainPage"));
 
-const ThemeConfig = {
-  initialColorMode: "system",
-  useSystemColorMode: false,
-  styles: {
-    global: {
-      "html, body, #root": {
-        height: "100% !important",
-      },
-      "input,textarea,button,select,a": {
-        WebkitTapHighlightColor: "transparent",
-      },
-    },
-  },
-};
-
 const App = () => {
+  const ThemeConfig = useMemo(
+    () => ({
+      initialColorMode: "system",
+      useSystemColorMode: false,
+      styles: {
+        global: {
+          "html, body, #root": {
+            height: "100% !important",
+          },
+          "input,textarea,button,select,a": {
+            WebkitTapHighlightColor: "transparent",
+          },
+        },
+      },
+    }),
+    []
+  );
   const theme = extendTheme(ThemeConfig);
   return (
     <ChakraProvider theme={theme}>
