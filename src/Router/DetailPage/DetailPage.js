04d823666ba343a0e93ce50258d3dcda265b189c
mohammadizanloo55 <mohammadizanloo55@gmail.com>
2021-08-10 12:11:09 +0430

    Added DetailPage

---
+++ src/Router/DetailPage/DetailPage.js
@@ -0,0 +1,111 @@
+import { Button, Flex, Grid, Heading, Image, Text } from "@chakra-ui/react";
+import loadable from "@loadable/component";
+import { memo, useEffect, useMemo, useState } from "react";
+import { HiArrowNarrowLeft } from "react-icons/hi";
+import { Link, useParams } from "react-router-dom";
+
+import FetchCountry from "../../Utilities/FetchCountry/FetchCountry";
+
+const Loading = loadable(() => import("../../Components/Loading/Loading"));
+
+const DetailPage = () => {
+  const { CountryName } = useParams();
+
+  const [CountryState, SetCountryState] = useState();
+
+  const NormalData = useMemo(
+    () =>
+      CountryState
+        ? [
+            { Data: CountryState.nativeName, Title: "Native Name" },
+            { Data: CountryState.population, Title: "Population" },
+            { Data: CountryState.region, Title: "Region" },
+            { Data: CountryState.subregion, Title: "Sub Region" },
+            { Data: CountryState.capital, Title: "Capital" },
+            {
+              Title: "Top Level Domain",
+              Data: CountryState.topLevelDomain.join(" , "),
+            },
+            {
+              Title: "Currencies",
+              Data: CountryState.currencies.map(({ name }) => name).join(" , "),
+            },
+            {
+              Title: "Languages",
+              Data: CountryState.languages.map(({ name }) => name).join(" , "),
+            },
+          ]
+        : undefined,
+    [CountryState]
+  );
+
+  useEffect(() => {
+    const FetchData = async () => {
+      const Country = await FetchCountry(
+        CountryName,
+        "nativeName;population;region;subregion;capital;topLevelDomain;currencies;languages;borders;alpha3Code"
+      );
+      SetCountryState({
+        ...Country.data,
+        name: CountryName,
+        population: parseInt(Country.data.population, 10).toLocaleString(),
+      });
+    };
+
+    FetchData();
+  }, [SetCountryState, CountryName]);
+
+  if (!NormalData) {
+    return <Loading />;
+  }
+
+  return (
+    <Grid
+      mt="10"
+      px="5"
+      templateColumns="repeat(1,auto)"
+      justifyContent="center"
+    >
+      <Link to="/">
+        <Button variant="solid" maxWidth="32">
+          <Text fontSize="2xl" mr="2">
+            <HiArrowNarrowLeft />
+          </Text>
+          Back
+        </Button>
+      </Link>
+
+      <Image mt="20" src={`${CountryState.flag}`} />
+
+      <Heading as="h1" mt="8" mb="4" fontSize="2xl">
+        {CountryName}
+      </Heading>
+      {NormalData.map(({ Data, Title }) => (
+        <Flex key={Data} my="1" alignItems="baseline">
+          <Text as="strong" fontSize="xl">
+            {Title}:
+          </Text>
+
+          <Text as="p" ml="2" fontSize="lg">
+            {Data}
+          </Text>
+        </Flex>
+      ))}
+
+      <Flex my="1" flexDirection="column">
+        <Text as="strong" fontSize="xl">
+          Border Countries :
+        </Text>
+        <Grid templateColumns="repeat(3,1fr)">
+          {CountryState.borders.map((Name) => (
+            <Button variant="outline" key={Name} m="2">
+              {Name}
+            </Button>
+          ))}
+        </Grid>
+      </Flex>
+    </Grid>
+  );
+};
+
+export default memo(DetailPage);
